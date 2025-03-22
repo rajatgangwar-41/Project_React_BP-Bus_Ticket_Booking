a433e81d06bf91b18b5ad8aa50fc754f9ebc2abb
Rajat Gangwar <rajat.gangwar41@gmail.com>
2025-03-22 17:53:08 +0530

    applied callback to ensure no-infinite renders

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,48 +1,72 @@
-import { useContext } from "react"
+import { useCallback, useContext } from "react"
 import { Context } from "../context/context"
 
 const useFilter = () => {
   const { state, dispatch } = useContext(Context)
 
-  const setOriginalData = (data) => {
-    dispatch({ type: "SET_ORIGINAL_DATA", payload: data })
-  }
+  const setOriginalData = useCallback(
+    (data) => {
+      dispatch({ type: "SET_ORIGINAL_DATA", payload: data })
+    },
+    [dispatch]
+  )
 
-  const setFilteredData = (data) => {
-    dispatch({ type: "SET_FILTERED_DATA", payload: data })
-  }
+  const setFilteredData = useCallback(
+    (data) => {
+      dispatch({ type: "SET_FILTERED_DATA", payload: data })
+    },
+    [dispatch]
+  )
 
-  const setUserTravelData = (data) => {
-    dispatch({ type: "SET_USER_TRAVEL_DATA", payload: data })
-  }
+  const setUserTravelData = useCallback(
+    (data) => {
+      dispatch({ type: "SET_USER_TRAVEL_DATA", payload: data })
+    },
+    [dispatch]
+  )
 
-  const setSearch = (data) => {
-    dispatch({ type: "SET_SEARCH", payload: data })
-  }
+  const setSearch = useCallback(
+    (data) => {
+      dispatch({ type: "SET_SEARCH", payload: data })
+    },
+    [dispatch]
+  )
 
-  const setPriceRange = (data) => {
-    dispatch({ type: "SET_PRICE_RANGE", payload: data })
-  }
+  const setPriceRange = useCallback(
+    (data) => {
+      dispatch({ type: "SET_PRICE_RANGE", payload: data })
+    },
+    [dispatch]
+  )
 
-  const setBusTypeList = (data) => {
-    dispatch({ type: "SET_BUS_TYPE_LIST", payload: data })
-  }
+  const setBusTypeList = useCallback(
+    (data) => {
+      dispatch({ type: "SET_BUS_TYPE_LIST", payload: data })
+    },
+    [dispatch]
+  )
 
-  const setBusCompanyList = (data) => {
-    dispatch({ type: "SET_BUS_COMPANY_LIST", payload: data })
-  }
+  const setBusCompanyList = useCallback(
+    (data) => {
+      dispatch({ type: "SET_BUS_COMPANY_LIST", payload: data })
+    },
+    [dispatch]
+  )
 
-  const setAmenitiesList = (data) => {
-    dispatch({ type: "SET_AMENITIES_LIST", payload: data })
-  }
+  const setAmenitiesList = useCallback(
+    (data) => {
+      dispatch({ type: "SET_AMENITIES_LIST", payload: data })
+    },
+    [dispatch]
+  )
 
-  const applyFilter = () => {
+  const applyFilter = useCallback(() => {
     dispatch({ type: "APPLY_FILTER" })
-  }
+  }, [dispatch])
 
-  const resetFilters = () => {
+  const resetFilters = useCallback(() => {
     dispatch({ type: "RESET" })
-  }
+  }, [dispatch])
 
   return {
     state,
